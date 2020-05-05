c48c22ae5981bac8cf98c45507089bd8c21c2637
Zakhar <zahgeek@gmail.com>
2020-05-05 20:00:57 +0300

    add Product entity class

---
+++ src/main/java/ua/khnu/entity/Product.java
@@ -0,0 +1,82 @@
+package ua.khnu.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private String color;
+    private String size;
+    private BigDecimal price;
+    private String photo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", color='" + color + '\'' +
+                ", size='" + size + '\'' +
+                ", price=" + price +
+                ", photo='" + photo + '\'' +
+                '}';
+    }
+}
