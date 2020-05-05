6b338512246183d514178fce4486118efc927c68
Zakhar <zahgeek@gmail.com>
2020-05-05 11:51:55 +0300

    finalize UserRepository and remove Repository interface

--- src/main/java/ua/khnu/reposetory/UserRepository.java
+++ src/main/java/ua/khnu/reposetory/UserRepository.java
@@ -2,9 +2,7 @@ package ua.khnu.reposetory;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ua.khnu.entity.User;
@@ -12,10 +10,12 @@ import ua.khnu.entity.User;
 import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
-public class UserRepository implements Repository<User> {
+public class UserRepository {
     private static final Logger LOG = Logger.getLogger(UserRepository.class);
     private static final String DELETE = "DELETE FROM users WHERE id=?";
     private static final String UPDATE = "UPDATE users " +
@@ -23,60 +23,44 @@ public class UserRepository implements Repository<User> {
             " WHERE id=?";
     private String[] columnNames;
     private JdbcTemplate jdbcAccessor;
-    private ApplicationContext ctx;
-    private RowMapper<User> rowMapper;
     private SimpleJdbcInsert simpleJdbcInsert;
 
 
     @Autowired
-    public UserRepository(JdbcTemplate jdbcAccessor, DataSource ds, ApplicationContext ctx) {
-        this.ctx = ctx;
+    public UserRepository(JdbcTemplate jdbcAccessor, DataSource ds) {
         this.jdbcAccessor = jdbcAccessor;
         initColumnNames();
-        initRowMapper();
         simpleJdbcInsert = new SimpleJdbcInsert(ds)
                 .withTableName("users").usingGeneratedKeyColumns("id")
                 .usingColumns(columnNames);
     }
 
-    @Override
     public User add(User user) {
         int id = simpleJdbcInsert.executeAndReturnKey(extractParameters(user)).intValue();
         user.setId(id);
         return user;
     }
 
-    @Override
-    public User query(String query) {
-        LOG.debug("query --> " + query);
-        return jdbcAccessor.queryForObject(query, rowMapper);
+    public List<User> query(String query) {
+        return query(query, (Object) null);
     }
 
-    @Override
+    public List<User> query(String query, Object... args) {
+        LOG.debug("query --> " + query);
+        LOG.debug("args --> " + Arrays.toString(args));
+        return getUserListFromResultList(jdbcAccessor.queryForList(query, args));
+    }
+
     public boolean delete(User user) {
         return jdbcAccessor.update(DELETE, user.getId()) > 0;
     }
 
-    @Override
     public boolean update(User user) {
         Object[] values = extractValues(user);
         Object[] newValues = Arrays.copyOf(values, values.length + 1);
         newValues[newValues.length - 1] = user.getId();
         LOG.debug("values --> " + Arrays.toString(newValues));
         return jdbcAccessor.update(UPDATE, newValues) > 0;
-    }
-
-    private void initRowMapper() {
-        rowMapper = (rs, rowNum) -> {
-            User user = ctx.getBean(User.class);
-            user.setId(rs.getInt("id"));
-            user.setLogin(rs.getString("login"));
-            user.setPassword(rs.getString("password"));
-            user.setEmail(rs.getString("email"));
-            user.setPaymentMethod(rs.getString("paymentMethod"));
-            user.setNumber(rs.getString("contactNumber"));
-            return user;
-        };
     }
 
     private Map<String, Object> extractParameters(User user) {
@@ -105,5 +89,18 @@ public class UserRepository implements Repository<User> {
         columnNames[2] = "email";
         columnNames[3] = "paymentMethod";
         columnNames[4] = "contactNumber";
+    }
+
+    private List<User> getUserListFromResultList(List<Map<String, Object>> resList) {
+        return resList.stream().map(m -> {
+            User user = new User();
+            user.setId((Integer) m.get("id"));
+            user.setLogin((String) m.get("login"));
+            user.setPassword((String) m.get("password"));
+            user.setEmail((String) m.get("email"));
+            user.setPaymentMethod((String) m.get("paymentMethod"));
+            user.setNumber((String) m.get("contactNumber"));
+            return user;
+        }).collect(Collectors.toList());
     }
 }
