339ca3293d03d4720a15d503b2194a8c2e3dd901
Sundeltop <47752324+Sundeltop@users.noreply.github.com>
2020-05-08 01:52:07 +0300

    added products load to main page

--- src/main/webapp/js/ajax.js
+++ src/main/webapp/js/ajax.js
@@ -17,47 +17,44 @@ function ajax_get(url, callback) {
     xmlhttp.send();
 }
 ajax_get('/getRandomProductOnMain', function(data) {
-    for (var i = 0; i <= data["Comics"].length-1; i++) {
+    for (var i = 0; i <= data["Фигурки"].length-1; i++) {
         var html = "<div class='card' id='card"+ i +"'>";
         html += "<a class='card-link' href='#''>";
-        html += "<div class='card-image-preview' id='card-image-preview' style='background-image=url(''"+ data["Comics"][i]["image"] + "'')'></div>";
+        html += "<div class='card-image-preview' id='card-image-preview' style='background-image: url("+ data["Фигурки"][i]["image"] + ")'></div>";
         html += "<div class='card-description'>";    
-        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Comics"][i]["title"] +"</h2>";
-        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Comics"][i]["price"] +"</h3>";                 
-        html +="</div></a></div>";
-        document.getElementById('content-card-holder-box3').innerHTML += html;
-        console.log(Object.keys(data).length);
-    }
-    for (var i = 0; i <= data["Statuette"].length-1; i++) {
-        var html = "<div class='card' id='card"+ i +"'>";
-        html += "<a class='card-link' href='#''>";
-        html += "<div class='card-image-preview' id='card-image-preview' style='background-image=url(''"+ data["Statuette"][i]["image"] + "'')'></div>";
-        html += "<div class='card-description'>";    
-        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Statuette"][i]["title"] +"</h2>";
-        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Statuette"][i]["price"] +"</h3>";                 
+        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Фигурки"][i]["title"] +"</h2>";
+        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Фигурки"][i]["price"] +"</h3>";                 
         html +="</div></a></div>";
         document.getElementById('content-card-holder-box1').innerHTML += html;
     }
-    for (var i = 0; i <= data["Tshirt"].length-1; i++) {
+    for (var i = 0; i <= data["Одежда"].length-1; i++) {
         var html = "<div class='card' id='card"+ i +"'>";
         html += "<a class='card-link' href='#''>";
-        html += "<div class='card-image-preview' id='card-image-preview' style='background-image=url(''"+ data["Tshirt"][i]["image"] + "'')'></div>";
+        html += "<div class='card-image-preview' id='card-image-preview' style='background-image: url("+ data["Одежда"][i]["image"] + ")'></div>";
         html += "<div class='card-description'>";    
-        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Tshirt"][i]["title"] +"</h2>";
-        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Tshirt"][i]["price"] +"</h3>";                 
+        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Одежда"][i]["title"] +"</h2>";
+        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Одежда"][i]["price"] +"</h3>";                 
         html +="</div></a></div>";
         document.getElementById('content-card-holder-box0').innerHTML += html;
     }
-    for (var i = 0; i <= data["Toy"].length-1; i++) {
+    for (var i = 0; i <= data["Игрушки"].length-1; i++) {
         var html = "<div class='card' id='card"+ i +"'>";
         html += "<a class='card-link' href='#''>";
-        html += "<div class='card-image-preview' id='card-image-preview' style='background-image=url(''"+ data["Toy"][i]["image"] + "'')'></div>";
+        html += "<div class='card-image-preview' id='card-image-preview' style='background-image: url("+ data["Игрушки"][i]["image"] + ")'></div>";
         html += "<div class='card-description'>";    
-        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Toy"][i]["title"] +"</h2>";
-        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Toy"][i]["price"] +"</h3>";                 
+        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Игрушки"][i]["title"] +"</h2>";
+        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Игрушки"][i]["price"] +"</h3>";                 
         html +="</div></a></div>";
         document.getElementById('content-card-holder-box2').innerHTML += html;
     }
-    
-
+    for (var i = 0; i <= data["Аксессуары"].length-1; i++) {
+        var html = "<div class='card' id='card"+ i +"'>";
+        html += "<a class='card-link' href='#''>";
+        html += "<div class='card-image-preview' id='card-image-preview' style='background-image: url("+ data["Аксессуары"][i]["image"] + ")'></div>";
+        html += "<div class='card-description'>";
+        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Аксессуары"][i]["title"] +"</h2>";
+        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Аксессуары"][i]["price"] +"</h3>";
+        html +="</div></a></div>";
+        document.getElementById('content-card-holder-box3').innerHTML += html;
+    }
 });
