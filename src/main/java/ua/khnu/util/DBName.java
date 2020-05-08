517c50a575332dc3f76f6d5b2c251c78e9a29a15
Zakhar <zahgeek@gmail.com>
2020-05-08 09:26:47 +0300

    add DBName annotation to provide availability to specify column name in DB for entity if it is different to field name

--- src/main/java/ua/khnu/util/DBName.java
+++ src/main/java/ua/khnu/util/DBName.java
@@ -1,7 +1,9 @@
 package ua.khnu.util;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DBName {
+    public String name();
 }
