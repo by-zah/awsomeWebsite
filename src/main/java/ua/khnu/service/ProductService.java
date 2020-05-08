24d2de7d40c05b0987fc05c413320aede7daac36
Zakhar <zahgeek@gmail.com>
2020-05-08 15:12:54 +0300

    rename constant and add GROUP BY

--- src/main/java/ua/khnu/service/ProductService.java
+++ src/main/java/ua/khnu/service/ProductService.java
@@ -18,7 +18,7 @@ public class ProductService {
     }
 
 
-    private static final String GET_4_RANDOM_PRODUCTS_FROM_CATEGORY="SELECT products_attributes.id AS id,\n" +
+    private static final String GET_RANDOM_PRODUCTS_FROM_CATEGORY ="SELECT products_attributes.id AS id,\n" +
             "       products.title AS title,\n" +
             "       categories.title AS category,\n" +
             "       products_attributes.photo AS photo,\n" +
@@ -29,16 +29,16 @@ public class ProductService {
             "         INNER JOIN categories\n" +
             "                    ON products.categoryID = categories.id\n" +
             "WHERE categories.title = ?\n" +
-          //  "GROUP BY products.title\n" +
+            "GROUP BY products.title\n" +
             "ORDER BY RAND()\n" +
             "LIMIT ?";
 
     public List<List<Product>> getRandomProductsFromEachCategories(int num) {
         List<List<Product>> res = new ArrayList<>();
-        res.add(repository.query(GET_4_RANDOM_PRODUCTS_FROM_CATEGORY,"Одежда",num));
-        res.add(repository.query(GET_4_RANDOM_PRODUCTS_FROM_CATEGORY,"Игрушки",num));
-        res.add(repository.query(GET_4_RANDOM_PRODUCTS_FROM_CATEGORY,"Фигурки",num));
-        res.add(repository.query(GET_4_RANDOM_PRODUCTS_FROM_CATEGORY,"Аксессуары",num));
+        res.add(repository.query(GET_RANDOM_PRODUCTS_FROM_CATEGORY,"Одежда",num));
+        res.add(repository.query(GET_RANDOM_PRODUCTS_FROM_CATEGORY,"Игрушки",num));
+        res.add(repository.query(GET_RANDOM_PRODUCTS_FROM_CATEGORY,"Фигурки",num));
+        res.add(repository.query(GET_RANDOM_PRODUCTS_FROM_CATEGORY,"Аксессуары",num));
         return res;
     }
 }
