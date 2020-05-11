89dc7442fd5ea0dda609d8b254f153b02b703f11
slamtheliquid <47554339+slamtheliquid@users.noreply.github.com>
2020-05-12 02:24:57 +0300

    Add files via upload
    2 scripts added

---
+++ src/main/webapp/js/catalog.js
@@ -0,0 +1,57 @@
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+ajax_get('/catalog', function(data) {
+    for (var i = 0; i <= data["Tshirt"].length-1; i++) {
+        var html = "<div class='card' id='card"+ i +"'>";
+        html += "<a class='card-link' href='#''>";
+        html += "<div class='card-image-preview' id='card-image-preview' style='background-image=url(''"+ data["Tshirt"][i]["image"] + "'')'></div>";
+        html += "<div class='card-description'>";    
+        html += "<h2 class='content-card-link-title' id='card-title'>"+ data["Tshirt"][i]["title"] +"</h2>";
+        html += "<h3 class='content-card-link-price' id='card-price'>"+ data["Tshirt"][i]["price"] +"</h3>";                 
+        html +="</div></a></div>";
+        document.getElementById('content-card-holder-box0').innerHTML += html;
+    }
+});
+function getAll() {
+    let url = new URL('http://localhost:8080/catalog.jsp?');
+    let params = new URLSearchParams(url.search.slice(1));
+    url.searchParams.set('sortType', $('#sortType').val());
+    $.each($('.search-checkbox:checked'), function() {
+        url.searchParams.append('category', $(this).val());
+        if($(this).val() == "Tshirt"){
+            $.each($("#color option:selected"), function() {
+                url.searchParams.append('color', $(this).val());
+            });
+            $.each($("#size option:selected"), function() {
+                url.searchParams.append('size', $(this).val());
+            });
+        }
+    });
+
+    if (!!$("#name").val()) {
+        url.searchParams.append('productName', $("#name").val());
+    }
+    if (!!$("#priceFrom").val()) {
+        url.searchParams.append('priceFrom', $("#priceFrom").val());
+    }
+    if (!!$("#priceTo").val()) {
+        url.searchParams.append('priceTo', $("#priceTo").val());
+    }
+    location.replace(url);
+}
---
+++ src/main/webapp/js/popup.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup").show();
+    //var popup = $("#popupback").addClass(".popup-wrapper-background");
+}
+function PopUpHide(){
+    $("#popup").hide();
+    //$(".popup-wrapper-background").removeClass(".popup-wrapper-background");
+}
+
+
+
