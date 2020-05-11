4e4bf8a5d0e2b8d6bc51fa0c5c459a5a0fde4f35
Zakhar <zahgeek@gmail.com>
2020-05-11 21:22:23 +0300

    Remove equals and hashCode

--- src/main/java/ua/khnu/entity/CatalogRequestParams.java
+++ src/main/java/ua/khnu/entity/CatalogRequestParams.java
@@ -90,22 +90,4 @@ public class CatalogRequestParams {
                 ", itemTo=" + itemTo +
                 '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CatalogRequestParams that = (CatalogRequestParams) o;
-        return sortType == that.sortType &&
-                Objects.equals(category, that.category) &&
-                Objects.equals(priceFrom, that.priceFrom) &&
-                Objects.equals(priceTo, that.priceTo) &&
-                Objects.equals(color, that.color) &&
-                Objects.equals(size, that.size);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(sortType, category, priceFrom, priceTo, color, size);
-    }
 }
